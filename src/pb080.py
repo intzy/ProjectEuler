c88372e00870a0a77531b2499ae2effd18c5d37a
Bryan Hill <bryan.r.hill@protonmail.com>
2021-10-07 06:44:17 -0600

    Add problem 080

---
+++ src/pb080.py
@@ -0,0 +1,22 @@
+"""
+Problem 080 of Project Euler.
+
+https://projecteuler.net/problem=080
+"""
+
+from math import isqrt
+
+from lib.euler_lib import is_square
+
+
+def problem080(num_digits=100, limit=100):
+    return sum(
+        square_root_digit_sum(n, num_digits)
+        for n in range(1, limit + 1)
+        if not is_square(n)
+    )
+
+
+def square_root_digit_sum(n, num_digits):
+    n *= 10 ** (2 * num_digits)
+    return sum(int(d) for d in str(isqrt(n))[:num_digits])
