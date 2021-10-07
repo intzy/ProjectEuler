8d4b64bc7be3238bd859f079611e4897c854ce25
Bryan Hill <bryan.r.hill@protonmail.com>
2021-10-07 08:25:18 -0600

    Complete problem 129

--- src/pb129.py
+++ src/pb129.py
@@ -4,6 +4,18 @@ Problem 129 of Project Euler.
 https://projecteuler.net/problem=129
 """
 
+from itertools import count
+from math import gcd
 
-def problem129():
-    pass
+
+def problem129(bound=1000000):
+    return next(n for n in count(bound) if gcd(n, 10) == 1 and A(n) > bound)
+
+
+def A(n):
+    c = 1
+    x = 1
+    while x:
+        x = (10 * x + 1) % n
+        c += 1
+    return c
