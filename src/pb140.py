0d2e8d2a5746aeabb63f970e8f7f5c22abe06a42
Bryan Hill <bryan.r.hill@protonmail.com>
2021-10-11 18:35:06 -0600

    Complete problems 137 and 140

--- src/pb140.py
+++ src/pb140.py
@@ -18,7 +18,18 @@ Therefore, we need to solve the generalized Pell's equation
 
     (5n + 7)^2 - 5m^2 = 44 = a^2 - 5b^2,
 
-where n = (a-7)/5 and m = b.
+where n = (a-7)/5 and m = b (hence, we require a = 2 (mod 5)).
+
+There are ways to solve this generalized Pell's equation.
+However, by seeing the first 12 or so golden nuggets,
+one can see that there are two equivalence classes of them,
+starting with [2, 21] and [5, 42], and the ratios
+a_n / a_{n - 1} converge to the same value (approx 6.8...) for each of these sequences,
+with the ratios decreasing monotonically.
+Therefore, we can just estimate the next term in the sequence,
+and search for the next golden nugget looking downwards.
+
+Thus, the above code solves the problem, though it is not rigorous.
 """
 
 from itertools import count
@@ -34,7 +45,6 @@ def problem140():
             m = n0 - d
             x = 5 * m * m + 14 * m + 1
             if is_square(x):
-                print(d)
                 n.append(m)
                 break
     n.append(5)
@@ -45,7 +55,6 @@ def problem140():
             m = n0 - d
             x = 5 * m * m + 14 * m + 1
             if is_square(x):
-                print(d)
                 n.append(m)
                 break
     return sum(n)
