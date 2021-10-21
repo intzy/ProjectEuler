dc15cd8a808b249f74505890bbe318f6b12a4fbc
Bryan Hill <bryan.r.hill@protonmail.com>
2021-10-21 02:11:50 -0600

    Start problem 108 and 110

---
+++ src/pb108.py
@@ -0,0 +1,9 @@
+"""
+Problem 108 of Project Euler.
+
+https://projecteuler.net/problem=108
+"""
+
+
+def problem108():
+    pass
---
+++ src/pb110.py
@@ -0,0 +1,9 @@
+"""
+Problem 110 of Project Euler.
+
+https://projecteuler.net/problem=110
+"""
+
+
+def problem110():
+    pass
