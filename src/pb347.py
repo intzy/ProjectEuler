425d0f816db89ce62654f0de9eb45b19c51dd113
Bryan Hill <bryan.r.hill@protonmail.com>
2021-11-07 00:43:27 -0600

    Complete problem 347

--- src/pb347.py
+++ src/pb347.py
@@ -4,6 +4,25 @@ Problem 347 of Project Euler.
 https://projecteuler.net/problem=347
 """
 
+from lib.euler_lib import list_primes
 
-def problem347():
-    pass
+
+def problem347(limit=10000000):
+    primes = list_primes(limit // 2)
+    solns = set()
+    for i, p in enumerate(primes):
+        if p * p > limit:
+            break
+        for q in primes[i + 1 :]:
+            if p * q > limit:
+                break
+            M = []
+            pp = p
+            while pp * q < limit:
+                qq = q
+                while pp * qq * q < limit:
+                    qq *= q
+                M.append(pp * qq)
+                pp *= p
+            solns.add(max(M))
+    return sum(solns)
