29a01debccdadfc3cf9b6172b11cd82286c56e0e
Bryan Hill <bryan.r.hill@protonmail.com>
2021-11-14 04:59:39 -0700

    Complete problem 122

--- src/pb122.py
+++ src/pb122.py
@@ -5,5 +5,23 @@ https://projecteuler.net/problem=122
 """
 
 
-def problem122():
-    pass
+def problem122(limit=200):
+    gotten = {1}
+    x = [(1,)]
+    L = [set(x)]
+    ans = 0
+    for i in range(1, 20):
+        lasts = set()
+        L.append(set())
+        for seq in L[i - 1]:
+            for c in seq:
+                nextt = seq[-1] + c
+                if nextt in gotten or nextt > limit:
+                    continue
+                lasts.add(nextt)
+                s = seq + (nextt,)
+                L[i].add(s)
+        for x in lasts:
+            gotten.add(x)
+        ans += i * len(lasts)
+    print(ans)
